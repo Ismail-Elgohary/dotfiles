9909f14b09299a33b6392357565f9a6a0a0bce0b
Elgohaary <ismailelgohary173@gmail.com>
2023-08-21 01:57:34 +0300

    update

--- .config/nvim/lua/Elgohary/autocmd.lua
+++ .config/nvim/lua/Elgohary/autocmd.lua
@@ -1,23 +1,13 @@
+local opt = vim.opt -- for conciseness 
 vim.cmd("autocmd!")
 
-vim.scriptencoding = "utf-8"
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
-
-vim.wo.number = true
-vim.opt.hlsearch = true
+opt.relativenumber = true
+opt.number = true
 vim.opt.backup = false
-vim.opt.showcmd = true
-vim.opt.cmdheight = 1
-vim.opt.laststatus = 2
 vim.opt.expandtab = true
-vim.opt.scrolloff = 10
-vim.opt.shell = "fish"
-vim.opt.backupskip = "/tmp/*,/private/tmp/*"
-vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.smarttab = true
-vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.ai = true -- Auto indent
@@ -28,8 +18,4 @@ vim.opt.backspace = "start,eol,indent"
 --hilight---
 vim.opt.cursorline = true
 vim.opt.termguicolors = true
-vim.opt.winblend = 0
-vim.opt.wildoptions = "pum"
-vim.opt.pumblend = 5
-vim.opt.background = "dark"
 
