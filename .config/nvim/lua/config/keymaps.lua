7c20a9eda4627b0c9328d7f1f93c39b78d605d16
Ismail Elgohary <ismailelgohary173@gmail.com>
2024-06-10 10:11:06 +0300

    neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,8 +3,7 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<C-d>",  "<C-d>zz")  --  first page
 vim.keymap.set("n", "<C-u>",  "<C-u>zz") --  end page
 vim.keymap.set("n", "<C-s>",  ":w<CR>")  -- Fast saving with <leader> and s
-
-
-
-
+vim.keymap.set({'n', 'x'}, '<leader>y', '"+y"')
+vim.keymap.set({'n', 'x'}, '<leader>p', '"+p:"')
 vim.keymap.set("n", "<leader>q", ":Ex<cr>")
+
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,9 +2,36 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.background = "dark"
 
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = false
 vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.g.netrw_banner = false
+vim.g.netrw_browse_split = false
+vim.g.netrw_altv = true
+vim.g.netrw_winsize = 25
+vim.g.netrw_fastbrowse = false
+
+vim.g.zig_fmt_autosave = false
+
+vim.g.netrw_banner = false
+vim.g.netrw_browse_split = false
+vim.g.netrw_altv = true
+vim.g.netrw_winsize = 25
+vim.g.netrw_fastbrowse = false
+
+vim.g.zig_fmt_autosave = false
+
+vim.g.loaded_node_provider = false
+vim.g.loaded_ruby_provider = false
+vim.g.loaded_perl_provider = false
+vim.g.loaded_python3_provider = false
 
