07dbf60fac613eb6a47744bf870d59e01a19adb6
Ismail Elgohary <ismailelgohary173@gmail.com>
2024-07-04 10:42:32 +0300

    colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,32 +1,7 @@
 return {
-	{
-		"AlexvZyl/nordic.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			local colors = require("nordic.colors")
-			require("nordic").setup({
-				transparent_bg = true,
-				override = {
-					StatusLine = {
-						fg = "none",
-					},
-					LineNr = {
-						fg = colors.gray5,
-					},
-					Visual = {
-						bg = colors.blue0,
-					},
-					PmenuSel = {
-						bg = colors.blue0,
-					},
-				},
-				telescope = {
-					style = "classic",
-				},
-			})
-			require("nordic").load()
-		end,
-	},
-}
-
+		"love-pengy/lillilac.nvim",
+		opts = { lazy = true },
+		config  = function(opts)
+				vim.cmd.colorscheme("lillilac")
+			end,
+		}
