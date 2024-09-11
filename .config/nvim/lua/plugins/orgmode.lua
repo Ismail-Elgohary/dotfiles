46bacbae95443489ea63e00a4ae3f819b2029782
Ismail Elgohary <ismailelgohary173@gmail.com>
2024-09-11 16:40:26 +0300

    remove deprecated function

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -3,10 +3,7 @@ return {
 	ft = "org",
 	event = "VeryLazy",
 	config = function()
-		require("orgmode").setup_ts_grammar()
 		require("orgmode").setup({
-			org_agenda_files = {"~/docs/notes/**/*", "~/docs/uni/**/*"},
-			org_default_notes_file = "~/docs/notes/refile.org",
 			org_todo_keywords = {"TODO(t)", "NEXT(n)", "WAITING(w)", "|", "DONE"},
 		})
 	end,
