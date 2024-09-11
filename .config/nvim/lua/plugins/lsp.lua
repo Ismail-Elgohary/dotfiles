f37ebf483e885d4364c11779cac8927037791d1e
Ismail Elgohary <ismailelgohary173@gmail.com>
2024-09-11 16:05:03 +0300

    fix tsserver to ts_ls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -26,6 +26,9 @@ return {
 			},
 			handlers = {
 				function(name)
+					if name ==  "tsserver" then
+					name = "ts_ls"
+					end
 					require("lspconfig")[name].setup({ capabilities = capabilities })
 				end,
 				["gopls"] = function()
